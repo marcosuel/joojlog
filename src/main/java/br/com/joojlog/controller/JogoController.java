bf807c1a7a8a43a7b8dbcb26ca2b9363c71ece55
Marcos Suel N. Saldanha <marcosuelns@gmail.com>
2020-12-07 09:55:10 -0300

    função de atualizar jogo adicionada

--- src/main/java/br/com/joojlog/controller/JogoController.java
+++ src/main/java/br/com/joojlog/controller/JogoController.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,5 +53,20 @@ public class JogoController {
 		}
 	}
 	
+	@PutMapping("/jogos/{id}")
+	public ResponseEntity<Jogo> atualizar(@PathVariable Long id, @RequestBody Jogo jogo){
+		try {
+			if(!jogoRep.existsById(id)) {
+				return ResponseEntity.notFound().build();
+			}
+			
+			jogo.setId(id);
+			jogo = jogoRep.save(jogo);
+			
+			return ResponseEntity.ok(jogo);
+		}catch (Exception e) {
+			return ResponseEntity.badRequest().build();
+		}
+	}
 	
 }
