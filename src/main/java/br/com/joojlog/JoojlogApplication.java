4957b45240eced0e0ed96048b56e325839b3be00
Marcos Suel N. Saldanha <marcosuelns@gmail.com>
2020-12-06 21:19:07 -0300

    Initial Commit

---
+++ src/main/java/br/com/joojlog/JoojlogApplication.java
@@ -0,0 +1,13 @@
+package br.com.joojlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoojlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoojlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/joojlog/JoojlogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joojlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoojlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
